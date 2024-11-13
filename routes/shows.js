3a544685374096592414aa3bc7e55ce4c0fbb569
John Arocha <john.arocha@clover.com>
2024-11-13 10:40:33 -0700

    added new show route and reordered endpooints for no confucion or mixups

--- routes/shows.js
+++ routes/shows.js
@@ -1,21 +1,31 @@
 const express = require('express')
 const showRouter = express.Router()
 const Show = require('../models/Show')
-const User = require('../models/User')
 
 showRouter.use(express.json())
 showRouter.use(express.urlencoded({ extended: true }))
+
+showRouter.get('/', async (req, res) => {
+    const allShows = await Show.findAll()
+    res.json(allShows)
+})
+
+showRouter.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const show = await Show.findByPk(id)
+    res.json(show)
+})
+
+showRouter.get('/genre/:genre', async (req, res) => {
+    const genre = req.params.genre
+    const show = await Show.findOne({ where: { genre } })
+    res.json(show)
+})
 
 showRouter.delete('/:id', async (req, res) => {
     const id = req.params.id
     await Show.destroy({ where: { id } })
     res.send('Show deleted')
-})
-
-showRouter.put('/:id/:property', async (req, res) => {
-    const id = req.params.id
-    const property = req.params.property
-    const updatedProperty = await Show.update({})// update the property via boolean, if its true make it false if false make it true
 })
 
 showRouter.put('/:id/:property', async (req, res) => {
@@ -30,17 +40,6 @@ showRouter.put('/:id/:property', async (req, res) => {
     } else {
         res.status(404).send('Show not found or property is not boolean')
     }
-})
-
-showRouter.get('/:id', async (req, res) => {
-    const id = req.params.id
-    const show = await Show.findByPk(id)
-    res.json(show)
-})
-
-showRouter.get('/', async (req, res) => {
-    const allShows = await Show.findAll()
-    res.json(allShows)
 })
 
 module.exports = showRouter
