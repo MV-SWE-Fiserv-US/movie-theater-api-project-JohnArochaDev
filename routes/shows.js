3e96d9f98c49dbbd8ae4937d226ce2d749dfe5c3
John Arocha <john.arocha@clover.com>
2024-11-13 10:32:28 -0700

    added put route for show

--- routes/shows.js
+++ routes/shows.js
@@ -1,9 +1,38 @@
 const express = require('express')
 const showRouter = express.Router()
 const Show = require('../models/Show')
+const User = require('../models/User')
 
 showRouter.use(express.json())
 showRouter.use(express.urlencoded({ extended: true }))
+
+showRouter.put()
+
+showRouter.put('/:id/:property', async (req, res) => {
+    const id = req.params.id
+    const property = req.params.property
+    const updatedProperty = await Show.update({})// update the property via boolean, if its true make it false if false make it true
+})
+
+showRouter.put('/:id/:property', async (req, res) => {
+    const id = req.params.id
+    const property = req.params.property
+
+    const show = await Show.findByPk(id)
+    if (show && typeof show[property] === 'boolean') {
+        show[property] = !show[property]
+        await show.save()
+        res.status(200).json(show)
+    } else {
+        res.status(404).send('Show not found or property is not boolean')
+    }
+})
+
+showRouter.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const show = await Show.findByPk(id)
+    res.json(show)
+})
 
 showRouter.get('/', async (req, res) => {
     const allShows = await Show.findAll()
